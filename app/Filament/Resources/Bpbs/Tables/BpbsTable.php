9c4b45b8e0e9c80f5cbf97ad36d7fa4f30e52961
Riyan Firmansyah <riyanfirmansyah048@gmail.com>
2026-03-31 08:21:02 +0700

    update

--- app/Filament/Resources/Bpbs/Tables/BpbsTable.php
+++ app/Filament/Resources/Bpbs/Tables/BpbsTable.php
@@ -19,22 +19,22 @@ class BpbsTable
     public static function configure(Table $table): Table
     {
         return $table
-            // ->query(
-            //     auth()->user()->hasRole('admin') // Periksa apakah user memiliki role "admin"
-            //         ? Bpb::query() // Jika admin, tampilkan semua data
-            //         : Bpb::query()->where('user_id', auth()->id()) // Jika bukan admin, hanya tampilkan miliknya sendiri
-            // )
             ->query(
-                auth()->user()->hasRole('admin')
-                    ? Bpb::query()->whereHas('purchase_order.bppb', function ($q) {
-                        $q->where('bppb_type_id', [1, 2]);
-                    })
-                    : Bpb::query()
-                    ->where('user_id', auth()->id())
-                    ->whereHas('purchase_order.bppb', function ($q) {
-                        $q->where('bppb_type_id', 1);
-                    })
+                auth()->user()->hasRole('admin') // Periksa apakah user memiliki role "admin"
+                    ? Bpb::query() // Jika admin, tampilkan semua data
+                    : Bpb::query()->where('user_id', auth()->id()) // Jika bukan admin, hanya tampilkan miliknya sendiri
             )
+            // ->query(
+            //     auth()->user()->hasRole('admin')
+            //         ? Bpb::query()->whereHas('purchase_order.bppb', function ($q) {
+            //             $q->where('bppb_type_id', [1, 2]);
+            //         })
+            //         : Bpb::query()
+            //         ->where('user_id', auth()->id())
+            //         ->whereHas('purchase_order.bppb', function ($q) {
+            //             $q->where('bppb_type_id', 1);
+            //         })
+            // )
             ->recordActionsPosition(RecordActionsPosition::BeforeColumns)
             ->columns([
                 // TextColumn::make('noBpb')
