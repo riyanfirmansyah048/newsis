ac3c90eb264a1d568d90d9ccef932655e7f32b32
Riyan Firmansyah <riyanfirmansyah048@gmail.com>
2026-04-23 09:13:02 +0700

    update validasi email Booking Order

--- app/Filament/Resources/BookingOrders/Pages/ListBookingOrders.php
+++ app/Filament/Resources/BookingOrders/Pages/ListBookingOrders.php
@@ -3,12 +3,31 @@
 namespace App\Filament\Resources\BookingOrders\Pages;
 
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\BookingOrders\BookingOrderResource;
 
 class ListBookingOrders extends ListRecords
 {
     protected static string $resource = BookingOrderResource::class;
+
+    public function mount(): void
+    {
+        parent::mount();
+
+        $email = trim((string) auth()->user()?->email);
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            Notification::make()
+                ->title('Email profile belum valid')
+                ->body('Lengkapi email pada profile Anda terlebih dahulu agar proses Booking Order berjalan normal. Anda akan diarahkan ke halaman profile.')
+                ->warning()
+                ->persistent()
+                ->send();
+
+            $this->redirectRoute('filament.sis.auth.profile');
+        }
+    }
 
     protected function getHeaderActions(): array
     {
