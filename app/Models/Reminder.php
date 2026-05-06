70be228a59d4ef57791822de7b7f0f28289952da
Riyan Firmansyah <riyanfirmansyah048@gmail.com>
2026-05-06 15:33:13 +0700

    LogsActivity reminder

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Reminder extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $guarded = [];
 
     protected $casts = [
         'expire_date' => 'date',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['*']);
+    }
 
     public function creator()
     {
